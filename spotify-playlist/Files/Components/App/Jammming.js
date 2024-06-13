d8a6b1df5a77c7c06a9562a983a8732a4623eda3
AbsentCoding <perez.javier321@gmail.com>
2024-06-13 14:37:27 -0700

    Working through static components

---
+++ spotify-playlist/Files/Components/App/Jammming.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+
+function Jammming() {
+    return (
+        <>
+            <div>
+                <span>
+
+                </span>
+                <span>
+
+                </span>
+                <span>
+                    
+                </span>
+            </div>
+        </>
+    )
+};
+
+export default Jammming;
