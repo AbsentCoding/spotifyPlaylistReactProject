11b3aaf0b68e9b5393cabf198fb3439ac9bd50ac
Javier Perez Padilla <144625395+AbsentCoding@users.noreply.github.com>
2024-06-20 20:17:45 -0700

    Update Tracklist.js
    added notes and export

--- spotify-playlist/Files/Components/Tracklist/Tracklist.js
+++ spotify-playlist/Files/Components/Tracklist/Tracklist.js
@@ -1,7 +1,10 @@
 import React, {useState} from 'react';
 
+// this function is to be exported to a parnt, most likely app.js//
 function Tracklist() {
   return (
     
   );
 };
+
+export default Tracklist;
