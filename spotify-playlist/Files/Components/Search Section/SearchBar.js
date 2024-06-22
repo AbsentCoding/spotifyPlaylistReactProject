0fa95b245b71c5dd704846b8a64c9345f20d7fa2
AbsentCoding <perez.javier321@gmail.com>
2024-06-21 18:33:45 -0700

    PC commit

--- spotify-playlist/Files/Components/Search Section/SearchBar.js
+++ spotify-playlist/Files/Components/Search Section/SearchBar.js
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
 
 function SearchBar() {
+    const [userInput, setUserInput] = useState("");
+
+    function handleUserInput(e) {
+        setUserInput(e.target.value);
+    };
+
+    function handleSubmit(e) {
+        
+    };
+
     return (
         <>
             <div>
-                <h2>
-                    Search
-                </h2>
+                <form onSubmit={handleSubmit}>
+                    
+                </form>
             </div>
         </>
     );
