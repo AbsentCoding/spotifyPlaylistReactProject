3a379483f67222b9526003bc1b8a54a5e9ae9243
Javier Perez Padilla <144625395+AbsentCoding@users.noreply.github.com>
2024-07-03 16:18:28 -0700

    Update SearchBar.js
    deleted file

--- spotify-playlist/Files/Components/Search Bar/SearchBar.js
+++ spotify-playlist/Files/Components/Search Bar/SearchBar.js
@@ -1,15 +1,1 @@
-import React, {useState} from 'react';
-import SearchButton from '//need to add the from//': 
 
-
-function SearchBar() {
-  return (
-    <>
-      <div>
-        <searchInput />
-      </div>
-    </>
-  );
-};
-
-export default SearchBar;
