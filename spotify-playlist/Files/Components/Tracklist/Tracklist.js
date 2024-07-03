4c6b48b9bd377e6bfb03f6c96bc7b92efbd4bbbf
Javier Perez Padilla <144625395+AbsentCoding@users.noreply.github.com>
2024-07-03 16:19:23 -0700

    Update Tracklist.js
    deleted contents

--- spotify-playlist/Files/Components/Tracklist/Tracklist.js
+++ spotify-playlist/Files/Components/Tracklist/Tracklist.js
@@ -1,10 +1,1 @@
-import React, {useState} from 'react';
 
-// this function is to be exported to a parnt, most likely app.js//
-function Tracklist() {
-  return (
-    
-  );
-};
-
-export default Tracklist;
